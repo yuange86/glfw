1c1def4f35f8de9962aa5dbd86ba379eef175f74
yuange86 <xvdjk0806@gmail.com>
2022-05-22 02:52:29 +0900

    uncompleted linux premake environment

--- premake5.lua
+++ premake5.lua
@@ -1,6 +1,6 @@
 project "GLFW"
 	kind "StaticLib"
-	language "C++"
+	language "C"
 
 	targetdir("%{wks.location}/" .. outputdir .. "/%{prj.name}")
     objdir("%{wks.location}/" .. outputdir .. "/.int/%{prj.name}")
@@ -16,26 +16,14 @@ project "GLFW"
 		"src/monitor.c",
 		"src/vulkan.c",
 		"src/window.c",
-
-		"src/internal.h",
-		"src/platform.h",
-		"src/mappings.h",
 		"src/platform.c",
-		"src/egl_context.c",
-		"src/osmesa_context.c",
-		"src/null_platform.h",
-		"src/null_joystick.h",
-		"src/null_init.c",
 
-		"src/null_monitor.c",
+		 -- later synchronize glfw/glfw repository
 		"src/null_window.c",
+		"src/null_monitor.c",
 		"src/null_joystick.c",
+		"src/null_init.c",
 	}
-
-	-- postbuildcommands
-	-- {
-	-- }
-
 	filter "system:linux"
 		pic "On"
 
@@ -45,15 +33,16 @@ project "GLFW"
 		files
 		{
 			"src/x11_init.c",
+			"src/linux_joystick.c",
 			"src/x11_monitor.c",
-			"src/x11_window.c",
-			"src/xkb_unicode.c",
 			"src/posix_time.c",
 			"src/posix_thread.c",
+			"src/x11_window.c",
+			"src/posix_module.c",
 			"src/glx_context.c",
 			"src/egl_context.c",
 			"src/osmesa_context.c",
-			"src/linux_joystick.c"
+			"src/xkb_unicode.c",
 		}
 
 		defines
@@ -73,13 +62,10 @@ project "GLFW"
 			"src/win32_time.c",
 			"src/win32_thread.c",
 			"src/win32_window.c",
+			"src/win32_module.c",
 			"src/wgl_context.c",
 			"src/egl_context.c",
 			"src/osmesa_context.c",
-
-			"src/win32_module.c",
-			"src/win32_time.h",
-			"src/win32_thread.h",
 		}
 
 		defines 
